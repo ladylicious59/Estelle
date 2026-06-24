956ae7b11013971e5cacf44dcee3a19e54a5730c
ladylicious59 <ladylicious59@users.noreply.github.com>
2026-06-24 19:25:46 +0000

    chore: correct .gitignore and remove unintended files from tracking

--- server/dist/routes/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import videoRoutes from './videoRoutes.js';
-import subscriptionRoutes from './subscriptionRoutes.js';
-import authRoutes from './authRoutes.js';
-const router = Router();
-router.use('/auth', authRoutes);
-router.use('/videos', videoRoutes);
-router.use('/subscriptions', subscriptionRoutes);
-export default router;
--- server/dist/routes/videoRoutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import * as videoController from '../controllers/videoController.js';
-const router = Router();
-router.get('/', videoController.listUserVideos);
-router.post('/generate', videoController.generateVideo);
-router.get('/:id', videoController.getVideoStatus);
-export default router;
