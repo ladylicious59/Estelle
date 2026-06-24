931323ec437819fcf87978c128dfa1f5679d84fd
ladylicious59 <ladylicious59@users.noreply.github.com>
2026-06-24 19:24:51 +0000

    chore: add .gitignore and remove node_modules from tracking

--- server/dist/routes/subscriptionRoutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import * as subscriptionController from '../controllers/subscriptionController.js';
-const router = Router();
-router.get('/status', subscriptionController.getSubscriptionStatus);
-router.post('/create-checkout', subscriptionController.createCheckoutSession);
-router.post('/cancel', subscriptionController.cancelSubscription);
-export default router;
